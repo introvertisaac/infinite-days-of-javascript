00ff398a9b5dcd16ef02b5ec1ff1c149ddc0467e
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2023-05-24 15:37:32 +0300

    variables

---
+++ variables/variables.js
@@ -0,0 +1,22 @@
+// you use the let keyword to create variables that can vary/change later
+
+let variable1=3;
+
+let sum=variable1 + 2;
+
+console.log(variable1);
+
+
+//variables of this type can be reassigned. 
+
+
+variable1=5;
+
+//when operating variables, you can either:
+
+variable1=variable1 + 3;
+
+//or 
+
+variable1+=3;
+
