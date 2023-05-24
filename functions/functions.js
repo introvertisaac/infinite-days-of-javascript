30fb7b982f10c116376027726c3446aef4c0040c
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2023-05-24 14:36:20 +0300

    hello

---
+++ functions/functions.js
@@ -0,0 +1,2 @@
+//functions use arrow symbols 
+
---
+++ strings/strings.js
@@ -0,0 +1,17 @@
+//strings are annotated by single quotation marks 
+
+// you can also use backticks if it involves other functions, quotation marks, or conditions. 
+
+
+alert `(${1+1}: $${(2095 + 799) / 100})`
+
+
+print('strings are annotated by single quotation marks');
+
+
+console.log('strings are annotated by single quotation marks');
+
+
+alert('strings are annotated by single quotation marks');
+
+
